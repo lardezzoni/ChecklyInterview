195f0a16415c34a7b5fd28e467ed48007dcaf25c
Luiz Ardezzoni <lhardezzoni@gmail.com>
2024-12-18 14:52:16 -0300

    security features on node server

--- ChecklyInterview/checkly.config.ts
+++ ChecklyInterview/checkly.config.ts
@@ -8,21 +8,21 @@ dotenv.config({ path: './.checkly.env' });
 export default defineConfig({
   projectName: 'ChecklyInterview',
   logicalId: 'checkly-interview-1',
-  repoUrl: 'https://github.com/lardezzoni/ChecklyInterview', // Replace with your repository URL
+  repoUrl: 'https://github.com/lardezzoni/ChecklyInterview', 
   checks: {
     activated: true,
     muted: false,
-    runtimeId: '2022.10', // Specify the runtime version
-    frequency: Frequency.EVERY_5M, // Default frequency for checks
-    locations: [], // Disable global locations
-    privateLocations: ['laptop-localhost'], // Replace with your private location slug
+    runtimeId: '2022.10', 
+    frequency: Frequency.EVERY_5M,
+    locations: [], 
+    privateLocations: ['laptop-localhost'], 
     tags: ['react', 'nodejs', 'checkly'],
-    checkMatch: '**/__checks__/**/*.check.ts', // Pattern to locate check files
+    checkMatch: '**/__checks__/**/*.check.ts', 
     ignoreDirectoriesMatch: [],
     browserChecks: {
-      frequency: Frequency.EVERY_10M, // Frequency for browser checks
-      testMatch: '**/__checks__/**/*.spec.ts', // Pattern to locate Playwright test files
-      privateLocations: ['laptop-localhost'], // Add private location for browser checks
+      frequency: Frequency.EVERY_10M, 
+      testMatch: '**/__checks__/**/*.spec.ts', 
+      privateLocations: ['laptop-localhost'], 
     },
   },
 })
